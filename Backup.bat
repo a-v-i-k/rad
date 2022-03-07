a0e7dc27164f248f8b1de8e9639eac5e184556af
Avi Kaplan <savik751@gmail.com>
2022-03-07 09:37:43 +0200

    RAD (Rooms and Doors)

--- Backup.bat
+++ Backup.bat
@@ -4,7 +4,7 @@ SET version-major=0
 SET version-minor=6
 
 rem OUTPUT FILE
-SET ZIPFILE=25rooms-v%version-major%.%version-minor%.zip
+SET ZIPFILE=rad-v%version-major%.%version-minor%.zip
 
 rem PRECLEAN
 if exist %ZIPFILE% (
@@ -12,7 +12,7 @@ if exist %ZIPFILE% (
 )
 
 rem ZIP
-"C:\Program Files\7-Zip\7z" a %ZIPFILE% ..\25rooms\
+"C:\Program Files\7-Zip\7z" a %ZIPFILE% ..\rad\
 
 rem BACKUP
-COPY /Y %ZIPFILE% C:\Users\kavi\Dropbox\avik\__backup__\web\25rooms\%ZIPFILE%
+COPY /Y %ZIPFILE% C:\Users\kavi\Dropbox\avik\__backup__\web\rad\%ZIPFILE%
